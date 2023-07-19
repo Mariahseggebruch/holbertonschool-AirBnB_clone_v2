cb648a5def5d44e60b763eb542b1baca4da6b099
Mariahseggebruch <mariahseggebruch@gmail.com>
2023-07-19 08:44:52 -0700

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,6 +4,7 @@ Write a script that starts a Flask web application:
 """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
